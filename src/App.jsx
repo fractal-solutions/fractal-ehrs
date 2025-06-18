ed3b8b64679e8a9a1052ee16265e2e84fe77aa92
fractal-solutions <fractal_solutions@protonmail.com>
2025-06-18 15:53:02 +0300

    new file:   .gitignore 	new file:   .hintrc 	new file:   README.md 	new file:   bun.lock 	new file:   components.json 	new file:   eslint.config.js 	new file:   index.html 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.css 	new file:   src/App.jsx 	new file:   src/assets/react.svg 	new file:   src/components/calendars.tsx 	new file:   src/components/date-picker.tsx 	new file:   src/components/nav-favorites.tsx 	new file:   src/components/nav-main.tsx 	new file:   src/components/nav-secondary.tsx 	new file:   src/components/nav-user.tsx 	new file:   src/components/nav-workspaces.tsx 	new file:   src/components/sidebar-left.tsx 	new file:   src/components/sidebar-right.tsx 	new file:   src/components/team-switcher.tsx 	new file:   src/components/ui/alert-dialog.tsx 	new file:   src/components/ui/alert.tsx 	new file:   src/components/ui/avatar.tsx 	new file:   src/components/ui/badge.tsx 	new file:   src/components/ui/breadcrumb.tsx 	new file:   src/components/ui/button.tsx 	new file:   src/components/ui/calendar.tsx 	new file:   src/components/ui/card.tsx 	new file:   src/components/ui/carousel.tsx 	new file:   src/components/ui/chart.tsx 	new file:   src/components/ui/checkbox.tsx 	new file:   src/components/ui/collapsible.tsx 	new file:   src/components/ui/command.tsx 	new file:   src/components/ui/context-menu.tsx 	new file:   src/components/ui/dialog.tsx 	new file:   src/components/ui/drawer.tsx 	new file:   src/components/ui/dropdown-menu.tsx 	new file:   src/components/ui/hover-card.tsx 	new file:   src/components/ui/input-otp.tsx 	new file:   src/components/ui/input.tsx 	new file:   src/components/ui/menubar.tsx 	new file:   src/components/ui/navigation-menu.tsx 	new file:   src/components/ui/pagination.tsx 	new file:   src/components/ui/popover.tsx 	new file:   src/components/ui/progress.tsx 	new file:   src/components/ui/radio-group.tsx 	new file:   src/components/ui/resizable.tsx 	new file:   src/components/ui/scroll-area.tsx 	new file:   src/components/ui/select.tsx 	new file:   src/components/ui/separator.tsx 	new file:   src/components/ui/sheet.tsx 	new file:   src/components/ui/sidebar.tsx 	new file:   src/components/ui/skeleton.tsx 	new file:   src/components/ui/slider.tsx 	new file:   src/components/ui/sonner.tsx 	new file:   src/components/ui/switch.tsx 	new file:   src/components/ui/table.tsx 	new file:   src/components/ui/tabs.tsx 	new file:   src/components/ui/textarea.tsx 	new file:   src/components/ui/toggle-group.tsx 	new file:   src/components/ui/toggle.tsx 	new file:   src/components/ui/tooltip.tsx 	new file:   src/hooks/use-mobile.ts 	new file:   src/index.css 	new file:   src/lib/utils.ts 	new file:   src/main.jsx 	new file:   src/pages/Frame.tsx 	new file:   tsconfig.app.json 	new file:   tsconfig.json 	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+import  Page  from "@/pages/Frame"
+
+function App() {
+  return (
+    <Page />
+  )
+}
+
+export default App
